68bdf4291c8647ea5349e6a0308614569c116386
Matheus Oliveira <129126932+Theusith@users.noreply.github.com>
2024-04-21 10:55:17 -0300

    Create Questão2.java

---
+++ Questão2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o numero:");
+        int numero = sc.nextInt();
+        function(numero);
+        sc.close();
+    }
+    public static void function(Integer n){
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
